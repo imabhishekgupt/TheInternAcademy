117742b8130f4f182a08e8ddb7a1435a95f6a5ff
Abhishek Gupta <89855934+imabhishekgupt@users.noreply.github.com>
2021-11-16 00:19:48 +0530

    Update plagiarism.py

--- Plagiarism/plagiarism.py
+++ Plagiarism/plagiarism.py
@@ -5,19 +5,19 @@
 
 
 # reading files
-file1 = open("file_1.txt",'r')
-file2 = open('file_2.txt','r')
+file1 = open("file1.txt",'r')
+file2 = open('file2.txt','r')
 
-file_1 = file1.readlines()
-file_2 = file2.readlines()
+file1 = file1.readlines()
+file2 = file2.readlines()
 
 #joining
-sen_file_1 = ''.join(file_1)
-sen_file_2 = ''.join(file_2)
+sen_file1 = ''.join(file1)
+sen_file2 = ''.join(file2)
 
 # spliting by lines
-file1ByLines = sen_file_1.split('.')
-file2ByLines = sen_file_2.split('.')
+file1ByLines = sen_file1.split('.')
+file2ByLines = sen_file2.split('.')
 
 plag = []
 
@@ -36,6 +36,6 @@ from difflib import SequenceMatcher
 
 print(" ")
 print(" ")
-Similarity = SequenceMatcher(None, file_1, file_2).ratio()
+Similarity = SequenceMatcher(None, file1, file2).ratio()
 print(f"Percentage by using of sequenceMatcher = {Similarity*100}%")
 print(f"Plagiarism percentage is manual matching = {percentage} %")
